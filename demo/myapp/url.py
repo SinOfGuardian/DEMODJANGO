cbb37d3f3aef6f7b5c17ac8da53720a435547198
Valenzuela, Nathaniel Rosell <99592583+SinOfGuardian@users.noreply.github.com>
2023-12-31 13:43:13 +0800

    Create apps
    done creating apps and connect the urls

---
+++ demo/myapp/url.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # "" = empty path string means go to the root
+    # views.home = go to views.py
+    # name="home" = name of the url
+    path("", views.home, name="home")
+]
