47d5fe7254ffbf359f363f97d7dd098c18f45db5
Valenzuela, Nathaniel Rosell <99592583+SinOfGuardian@users.noreply.github.com>
2023-12-31 15:10:12 +0800

    Create Model and Migrate
    creating model and make migration

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # "" = empty path string means go to the root
     # views.home = go to views.py
     # name="home" = name of the url
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("todos/", views.todos, name="Todos")
 ]
